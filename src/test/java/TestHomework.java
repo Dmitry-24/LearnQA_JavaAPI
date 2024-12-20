887649dabf4d069b0946109c430456d50ba31beb
dmitr <Dmitry-perfilev94@yande.ru>
2024-12-20 20:42:26 +0300

    Ex6

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -1,6 +1,11 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestHomework {
 
@@ -21,5 +26,28 @@ public class TestHomework {
         System.out.println(answer);
 
     }
+
+
+    @Test
+    public void homeworkEx6(){
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String location = response.getHeader("Location");
+        System.out.println(location);
+
+    }
+
+
+
+
+
+
 
 }
