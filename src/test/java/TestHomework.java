3b94db065ba2302fe5603c876f182b1a6204149e
dmitr <Dmitry-perfilev94@yande.ru>
2024-12-21 10:01:09 +0300

    Ex8

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -1,22 +1,23 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class TestHomework {
 
     @Test
-    public void testHelloFrom(){
+    public void testHelloFrom() {
         System.out.println("Hello from Dmitry!");
     }
 
 
     @Test
-    public void homeworkEx5(){
+    public void homeworkEx5() {
 
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
@@ -29,7 +30,7 @@ public class TestHomework {
 
 
     @Test
-    public void homeworkEx6(){
+    public void homeworkEx6() {
 
         Response response = RestAssured
                 .given()
@@ -46,7 +47,7 @@ public class TestHomework {
 
 
     @Test
-    public void homeworkEx7(){
+    public void homeworkEx7() {
 
         Response response = RestAssured
                 .given()
@@ -62,7 +63,7 @@ public class TestHomework {
         System.out.println(location);
 
         while (statusCode != 200) {
-                     response = RestAssured
+            response = RestAssured
                     .given()
                     .redirects()
                     .follow(false)
@@ -77,6 +78,46 @@ public class TestHomework {
         }
     }
 
+    @Test
+    public void homeworkEx8() throws InterruptedException {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        int sec = response.get("seconds");
+        int millsec = sec * 1000;
+        String token = response.get("token");
+        System.out.println("Времени до завершения задачи " + sec + " сек");
+        System.out.println("Значение токена = " + token);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("token", token);
+        JsonPath responseStatus = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        String status = responseStatus.get("status");
+        String result = responseStatus.get("result");
+
+        if (!Objects.equals(status, "Job is ready")) {
+            System.out.println("Ожидание...");
+            Thread.sleep(millsec);
+            responseStatus = RestAssured
+                    .given()
+                    .queryParams(params)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+            result = responseStatus.get("result");
+            status = responseStatus.get("status");
+            System.out.println("Текущий статус задачи = " + status);
+            System.out.println("Результат задачи = " + result);
+        } else {
+            System.out.println("Текущий статус задачи = " + status);
+            System.out.println("Результат задачи = " +result);
+        }
+
+    }
 
 }
 
