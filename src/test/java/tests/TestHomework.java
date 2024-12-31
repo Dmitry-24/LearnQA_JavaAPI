48a966a056c68ade72dd95543b6d9319d9626673
dmitr <Dmitry-perfilev94@yande.ru>
2024-12-31 08:20:11 +0300

    homework Ex13

--- src/test/java/tests/TestHomework.java
+++ src/test/java/tests/TestHomework.java
@@ -5,6 +5,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import javax.swing.*;
 
@@ -119,11 +121,10 @@ public class TestHomework extends BaseTestCase {
             System.out.println("Результат задачи = " + result);
         } else {
             System.out.println("Текущий статус задачи = " + status);
-            System.out.println("Результат задачи = " +result);
+            System.out.println("Результат задачи = " + result);
         }
 
     }
-
 
 
     @Test
@@ -198,7 +199,7 @@ public class TestHomework extends BaseTestCase {
 
         String text = "Java it's cool!";
         int lengthText = text.length();
-        assertEquals(15,lengthText,"Text more than 15 characters ");
+        assertEquals(15, lengthText, "Text more than 15 characters ");
     }
 
 
@@ -215,8 +216,6 @@ public class TestHomework extends BaseTestCase {
     }
 
 
-
-
     @Test
     public void homeworkEx12() {
 
@@ -230,8 +229,34 @@ public class TestHomework extends BaseTestCase {
     }
 
 
+    @ParameterizedTest
+    @CsvSource(delimiterString = "|",
+            value = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 | Mobile | No | Android",
+                    "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1 | Mobile | Chrome | iOS",
+                    "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) | Googlebot | Unknown | Unknown",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0 | Web | Chrome | No",
+                    "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 | Mobile | No | iPhone"
+            })
+
+
+    public void homeworkEx13(String userAgent, String Platform, String Browser, String Device) {
+        JsonPath response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        assertEquals(Device, response.getString("device"), "Wrong device");
+        assertEquals(Browser, response.getString("browser"), "Wrong browser");
+        assertEquals(Platform, response.getString("platform"), "Wrong platform");
+
+
+    }
+
 
 }
+
+
 
 
 
