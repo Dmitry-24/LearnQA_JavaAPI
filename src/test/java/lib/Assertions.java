e263307280537beaa9455e0619f1459c437495c8
dmitr <Dmitry-perfilev94@yande.ru>
2025-01-04 10:22:52 +0300

    IV 01 and 02

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.hamcrest.Matchers.hasKey;
 
 public class Assertions {
+
+
+
     public static void assertJsonByName(Response Response, String name, int expectedValue) {
         Response.then().assertThat().body("$", hasKey(name));
 
@@ -13,5 +16,20 @@ public class Assertions {
         assertEquals(expectedValue, value, "JSON value is not to expected value");
 
     }
+
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer) {
+        assertEquals(expectedAnswer, Response.asString(), "Response text is not as expected");
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode) {
+        assertEquals(expectedStatusCode, Response.statusCode(), "Response status code is not as expected");
+    }
+
+
+    public static void assertJsonHasKey(Response Response, String expectedFieldName) {
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
 
 }
